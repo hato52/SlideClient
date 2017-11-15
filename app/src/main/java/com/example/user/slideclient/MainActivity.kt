ca5adfe5c8606ae7f2e21919b0981ab3d9828245
hato <hato.gt@gmail.com>
2017-11-16 00:05:19 +0900

    update 11/16

--- app/src/main/java/com/example/user/slideclient/MainActivity.kt
+++ app/src/main/java/com/example/user/slideclient/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.user.slideclient
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.PendingIntent.getActivity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
 import android.bluetooth.BluetoothSocket
 import android.content.Intent
 import android.os.AsyncTask
@@ -16,23 +18,26 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.*
+import java.util.UUID.fromString
 
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_ENABLE_BT = 1
-    //val myDevice: BluetoothDevice? = null
-    var mySocket: BluetoothSocket? = null
-    var myInStream: InputStream? = null
-    var myOutStream: OutputStream? = null
+    private val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
-    var serverNameText: TextView = findViewById<TextView>(R.id.select_server)
+    private var global_mySocket: BluetoothSocket? = null
+    private var serverNameText: TextView? = null
+    private val myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        serverNameText = findViewById<TextView>(R.id.select_server)
+
         /**アプリ起動時にBluetooth有効かをチェック*/
-        val myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        //val myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (myBluetoothAdapter == null) {
             Toast.makeText(this, "この端末では使えないよ(´・ω・`)", Toast.LENGTH_SHORT).show()
         }
@@ -56,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         /**サーバ端末へデータを送信*/
         val send_button: Button = findViewById<Button>(R.id.send_button) as Button
         send_button.setOnClickListener{ view ->
-            communicationThreadExec(mySocket, )  /**データ送信*/
+            communicationThreadExec(global_mySocket, "TEST_DATA")  /**データ送信*/
             //Toast.makeText(this, "send data", Toast.LENGTH_SHORT).show()
         }
     }
@@ -96,50 +101,64 @@ class MainActivity : AppCompatActivity() {
          * ダイアログを閉じてスレッドを実行
          * */
         listView.setOnItemClickListener{parent,view, position, id ->
-            serverNameText.text = deviceName[position]
+            serverNameText?.text = deviceName[position]
             dialog.dismiss()
-            connectThreadExec(device[position]) /**サーバ端末に接続する*/
+            acceptThreadExec() /**サーバ端末に接続する*/
         }
     }
 
     /**サーバ端末に接続するスレッド */
-    private fun connectThreadExec(device: BluetoothDevice) {
+    private fun acceptThreadExec() {
 
-        if (device == null) {
-            Toast.makeText(this, "デバイスが存在しません", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        val MY_UUID: UUID = UUID.fromString("a294fa24-c133-4875-86ac-0a0fe266a25a")
+        var myServerSocket: BluetoothServerSocket?
+        var tmp: BluetoothServerSocket? = null
 
         try {
-            mySocket = device.createRfcommSocketToServiceRecord(MY_UUID)
+            tmp = myBluetoothAdapter.listenUsingRfcommWithServiceRecord(this.packageName, MY_UUID)
         } catch (e: IOException) {
             println(e.printStackTrace())
         }
+        myServerSocket = tmp
 
-        object : AsyncTask<Void, Void, Boolean>() {
-            override fun doInBackground(vararg params: Void): Boolean {
-                try {
-                    mySocket?.connect()
-                } catch (connectException: IOException) {
+        Toast.makeText(this, "接続受け入れを開始", Toast.LENGTH_SHORT).show()
+
+        object : AsyncTask<Void, Void, BluetoothSocket?>() {
+
+            var mySocket: BluetoothSocket? = null
+
+            override fun doInBackground(vararg params: Void): BluetoothSocket? {
+                var mySocket: BluetoothSocket? = null
+
+                while(true) {
                     try {
-                        mySocket?.close()
-                    } catch (closeException: IOException) {
-                        println(closeException.printStackTrace())
+                        mySocket = myServerSocket?.accept()
+                    } catch (connectException: IOException) {
+                        return null
+                        break
                     }
-                    return false
+
+                    if(mySocket != null) {
+                        myServerSocket?.close()
+                        return mySocket
+                        break
+                    }
                 }
-                return true
             }
 
-            override fun onPostExecute(result: Boolean) {
-                if (result) {
+            override fun onPostExecute(result: BluetoothSocket?) {
+                if (result != null) {
                     Toast.makeText(this@MainActivity, "接続に成功", Toast.LENGTH_SHORT).show()
                     Log.d("SUCCESS!!", "接続に成功")
+                    global_mySocket = result
+                    try {
+                        mySocket?.close()
+                    } catch (e: IOException) {
+                        println(e.printStackTrace())
+                    }
                 }else{
                     Toast.makeText(this@MainActivity, "接続に失敗", Toast.LENGTH_SHORT).show()
                     Log.d("FAILED!!", "接続に失敗")
+                    global_mySocket = null
                     try {
                         mySocket?.close()
                     } catch (e: IOException) {
@@ -147,14 +166,19 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+
+            override fun onCancelled() {
+                super.onCancelled()
+                mySocket?.close()
+            }
         }.execute()
     }
 
-    /**サーバ端末へのデータの送信を行うスレッド*/
-    private fun communicationThreadExec(socket: BluetoothSocket?, send_data: ByteArray) {
-        //private val mySocket: BluetoothSocket? = socket
-        //private var myInStream: InputStream? = null
-        //private var myOutStream: OutputStream? = null
+    /**クライアント端末へのデータの送信を行うスレッド*/
+    private fun communicationThreadExec(socket: BluetoothSocket?, send_data: String) {
+        val mySocket: BluetoothSocket? = socket
+        var myInStream: InputStream? = null
+        var myOutStream: OutputStream? = null
 
         if (socket == null) {
             Toast.makeText(this, "ソケットが存在しません", Toast.LENGTH_SHORT).show()
@@ -196,6 +220,11 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-        }.execute(send_data)
+
+            override fun onCancelled() {
+                super.onCancelled()
+                mySocket?.close()
+            }
+        }.execute(send_data.toByteArray())
     }
 }
